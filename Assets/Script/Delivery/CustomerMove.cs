6e8c92079583c133c506a41fb5546f507e91d845
Philps <yunok96@naver.com>
2020-10-03 14:55:07 +0900

    손님 애니메이션 수정 & 퇴마사 프리팹 추가
    손님 이동중에 애니메이션 디폴트로 고정시킴
    
    퇴마사 애니메이션 추가한 프리팹 추가

--- Assets/Script/Delivery/CustomerMove.cs
+++ Assets/Script/Delivery/CustomerMove.cs
@@ -1,13 +1,24 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class CustomerMove : MonoBehaviour
 {
     public bool isGetFood = false;
     public Vector3 targetTable;
     public int whoIsMe;
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
         transform.position = (isGetFood) ? Vector3.MoveTowards(transform.position, new Vector3(-17, transform.position.y, 0), 3 * Time.deltaTime) : Vector3.MoveTowards(transform.position, targetTable, 3 * Time.deltaTime);
+        if (transform.position == targetTable)
+            anim.SetBool("GetPoint", true);
+        else
+            anim.SetBool("GetPoint", false);
     }
 }
